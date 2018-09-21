3f8cb03f9018a2e71bee91bc74169a45ccffe4d1
Huakun Xie <neil@Huakuns-MacBook-Pro.local>
2018-09-20 21:44:24 -0400

    Add new initial component files and package.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import React from 'react' 
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Youtube api key
+const API_KEY = 'AIzaSyAXjJuo7hdayJ-e9Md9Ki3YYJJqMso2Ve0';
+
 //create a new component whcih will generate HTML 
 const App = () => {
     return <div>My first react app!</div>;
