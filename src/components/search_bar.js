218cb213254046237dd6c584842dc9f276ac4911
Huakun Xie <neil@Huakuns-MacBook-Pro.local>
2018-11-11 21:26:06 -0500

    Finish search bar component.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    return <input />; //React.createElement
-};
+//const SearchBar = () => {
+//    return <input />; //React.createElement
+//};
 
- 
+// new class named searchbar with all access to react components
+class SearchBar extends Component {
+    constructor(props){
+        super(props); //call parent methods
+         
+        //define states, only in class component
+        this.state = {term: ''}; //only in constructor, change state like this, or use setState
+    }
+
+    //every class must have a render method
+    render(){
+        //return <input onChange={this.onInputChange} />;
+        //setState will re-render the component
+        return (
+            <div>
+                <input value={this.state.term}
+                onChange={event => this.setState({term:event.target.value})} />
+                <br />
+                Value of the input: {this.state.term}
+            </div>
+        );
+    }
+
+    //onInputChange(event){
+    //    console.log(event.target.value);
+    //}
+}
+
+ export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//for library, no need to specify the path because it's unique
+//for import from file, need to specify path because files can be multiple with same name
+import SearchBar from './components/search_bar';
 
 // Youtube api key
 const API_KEY = 'AIzaSyAXjJuo7hdayJ-e9Md9Ki3YYJJqMso2Ve0';
 
 //create a new component whcih will generate HTML 
 const App = () => {
-    return <div>My first react app!</div>;
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
 }
 
 //take this component's generated HTML and put it on the page (in the dom)
