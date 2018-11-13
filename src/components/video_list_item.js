3f823dd6c595d36b70d952078948578f9fd5100a
Huakun Xie <neil@Huakuns-MacBook-Pro.local>
2018-11-12 23:16:04 -0500

    Add video list for the search result

---
+++ src/components/video_list_item.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+    const imageUrl = video.snippet.thumbnails.default.url;
+    return (
+        <li className="list-group-item"> 
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="meida-object" src={imageUrl}></img>
+                </div>
+            </div>
+
+            <div className="media-body">
+                <div className="media-heading">{video.snippet.title}</div>
+            </div>
+        </li>
+    );
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 //for library, no need to specify the path because it's unique
+
+import YTSearch from 'youtube-api-search'
+
 //for import from file, need to specify path because files can be multiple with same name
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 // Youtube api key
 const API_KEY = 'AIzaSyAXjJuo7hdayJ-e9Md9Ki3YYJJqMso2Ve0';
 
 //create a new component whcih will generate HTML 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {videos:[]};
+
+        YTSearch({key: API_KEY, term: 'itsRae'}, (videos) => {
+            this.setState({ videos }); //equals to this. setState({videos:videos});
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} /> 
+            </div>
+        );
+    }
 }
 
 //take this component's generated HTML and put it on the page (in the dom)
